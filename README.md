13d057278d42e4bb1eab881ef3cb165280e8fdb7
andromeda <andromeda@lenovo>
2026-01-24 18:16:40 +0100

    license, min build?

---
+++ README.md
@@ -0,0 +1,2 @@
+`nix build` or `nix run` to build lib or run the example
+to do it manually, run `./generate.sh` to generate `lib/` before `cabal build` or `cabal run`
--- flake.nix
+++ flake.nix
@@ -12,9 +12,21 @@
   }: let
     version = "0.1.0";
     package = {
+      # nix stuff
       mkDerivation,
       lib,
+      # haskell deps
       base,
+      c-expr-runtime,
+      hs-bindgen-runtime,
+      # pkgconfig deps
+      libGL,
+      libX11,
+      libXcursor,
+      libXi,
+      xrandr,
+      # shell deps
+      hs-bindgen-cli,
     }:
       mkDerivation {
         pname = "hs-rgfw";
@@ -22,11 +34,24 @@
         src = ./.;
         libraryHaskellDepends = [
           base
+          c-expr-runtime
+          hs-bindgen-runtime
         ];
         libraryPkgconfigDepends = [
+          libGL
+          libX11
+          libXcursor
+          libXi
+          xrandr
         ];
+        preBuild = ''
+          set -x
+          export PATH=${hs-bindgen-cli}/bin:\$PATH
+          ./generate.sh
+          set +x
+        '';
         homepage = "https://git.mtgmonkey.net/Andromeda/hs-rgfw";
-        license = lib.licenses.bsd3;
+        license = lib.licenses.gpl3Only;
         platforms = ["x86_64-linux"];
       };
     system = "x86_64-linux";
