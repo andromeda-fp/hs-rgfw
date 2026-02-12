cea8ba33545149438a9f4372279439cb8a8b1fba
andromeda <andromeda@lenovo>
2026-02-12 22:20:30 +0100

    it finally compiles :)

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,5 @@
 module Main where
 
-import qualified Foreign.Ptr (ToFunPtr)
-
-import qualified RGFW.Generated as RGFW
-
 main :: IO ()
 main = do
   putStrLn "Hello World"
-  window <- toFunPtr ()
